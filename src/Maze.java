f97eb788cdf9918a9e617d12e7a3b51203e08a6a
Franck Brazier <franck.brazier@telecom-paristech.fr>
2013-11-13 09:37:10 +0100

    Début de la méthode voisinsde

--- src/Maze.java
+++ src/Maze.java
@@ -2,4 +2,17 @@ import java.util.ArrayList;
 
 public class Maze {
 	private ArrayList<MBox> mazeList;
+	
+	public ArrayList<MBox> estVoisinDe(int x,int y){
+		ArrayList<MBox> voisins = new ArrayList<MBox>();
+		switch (x){
+		case 0 : 
+			switch (y){
+			case 0 : voisins.add(mazeList.get(1));
+			         voisins.add(mazeList.get(10));
+				
+			}
+			}
+		}
+	}
 }
