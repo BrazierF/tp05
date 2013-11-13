7684dfbf7eb91db3953dda790bfdac0120bea60d
Franck Brazier <franck.brazier@telecom-paristech.fr>
2013-11-13 09:58:02 +0100

    Cas ou x appartient a 0-8 dans voisinsde

--- src/Maze.java
+++ src/Maze.java
@@ -5,14 +5,38 @@ public class Maze {
 	
 	public ArrayList<MBox> estVoisinDe(int x,int y){
 		ArrayList<MBox> voisins = new ArrayList<MBox>();
+		int z = 10*x + y;
 		switch (x){
 		case 0 : 
 			switch (y){
-			case 0 : voisins.add(mazeList.get(1));
-			         voisins.add(mazeList.get(10));
-				
-			}
+			case 0 : voisins.add(mazeList.get(z+1));
+			         voisins.add(mazeList.get(z+10));
+			         break;
+			case 9:  voisins.add(mazeList.get(z-1));
+	                 voisins.add(mazeList.get(z+10));
+	                 break;
+	        default : voisins.add(mazeList.get(z-1));
+	                  voisins.add(mazeList.get(z+1));
+	                  voisins.add(mazeList.get(z+10));
+	                  break;
+					}
+		 default :
+			 switch (y){
+				case 0 : voisins.add(mazeList.get(z+1));
+				         voisins.add(mazeList.get(z+10));
+				         voisins.add(mazeList.get(z-10));
+				         break;
+				case 9:  voisins.add(mazeList.get(z-1));
+		                 voisins.add(mazeList.get(z+10));
+		                 voisins.add(mazeList.get(z-10));
+		                 break;
+		        default : voisins.add(mazeList.get(z-1));
+		                  voisins.add(mazeList.get(z+1));
+		                  voisins.add(mazeList.get(z+10));
+		                  voisins.add(mazeList.get(z-10));
+		                  break;
+						}
 			}
 		}
-	}
+	
 }
