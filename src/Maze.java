7013ba00c47f5c1ab65aeb303fb1bb5a425e29ae
Yogananthan <lefanadeff@hotmail.fr>
2013-11-14 10:40:59 +0100

    Utilisation d'une matrice pour représenter le labyrinthe. Rectification d'une erreur sur le return

--- src/Maze.java
+++ src/Maze.java
@@ -1,58 +1,60 @@
 import java.util.ArrayList;
 
 public class Maze {
-	private ArrayList<MBox> mazeList;
+	private MBox[][] mazeMatrix;
 
 	public ArrayList<MBox> estVoisinDe(int x, int y) {
 		ArrayList<MBox> voisins = new ArrayList<MBox>();
-		int z = 10*x + y;
+		/*int z = 10*x + y;*/
 		switch (x){
 		case 0 : 
 			switch (y){
-			case 0 : voisins.add(mazeList.get(z+1));
-			         voisins.add(mazeList.get(z+10));
+			case 0 : voisins.add(mazeMatrix[x][y+1]);
+			         voisins.add(mazeMatrix[x+1][y]);
 			         break;
-			case 9:  voisins.add(mazeList.get(z-1));
-	                 voisins.add(mazeList.get(z+10));
+			case 9:  voisins.add(mazeMatrix[x][y-1]);
+	                 voisins.add(mazeMatrix[x+1][y]);
 	                 break;
-	        default : voisins.add(mazeList.get(z-1));
-	                  voisins.add(mazeList.get(z+1));
-	                  voisins.add(mazeList.get(z+10));
+	        default : voisins.add(mazeMatrix[x+1][y]);
+	                  voisins.add(mazeMatrix[x-1][y]);
+	                  voisins.add(mazeMatrix[x][y+1]);
 	                  break;
 					}
 		case 9:
 			switch (y) {
 			case 0:
-				voisins.add(mazeList.get(z - 10));
-				voisins.add(mazeList.get(z + 1));
+				voisins.add(mazeMatrix[x][y+1]);
+				voisins.add(mazeMatrix[x-1][y]);
 				break;
 			case 9:
-				voisins.add(mazeList.get(z - 10));
-				voisins.add(mazeList.get(z - 1));
+				voisins.add(mazeMatrix[x][y-1]);
+				voisins.add(mazeMatrix[x-1][y]);
 				break;
 			default:
-				voisins.add(mazeList.get(z - 1));
-				voisins.add(mazeList.get(z + 1));
-				voisins.add(mazeList.get(z - 10));
+				voisins.add(mazeMatrix[x][y-1]);
+				voisins.add(mazeMatrix[x][y+1]);
+				voisins.add(mazeMatrix[x-1][y]);
 			}
 		 default :
 			 switch (y){
-				case 0 : voisins.add(mazeList.get(z+1));
-				         voisins.add(mazeList.get(z+10));
-				         voisins.add(mazeList.get(z-10));
+				case 0 : voisins.add(mazeMatrix[x+1][y]);
+				         voisins.add(mazeMatrix[x-1][y]);
+				         voisins.add(mazeMatrix[x][y+1]);
 				         break;
-				case 9:  voisins.add(mazeList.get(z-1));
-		                 voisins.add(mazeList.get(z+10));
-		                 voisins.add(mazeList.get(z-10));
+				case 9:  voisins.add(mazeMatrix[x+1][y]);
+		                 voisins.add(mazeMatrix[x-1][y]);
+		                 voisins.add(mazeMatrix[x][y-1]);
 		                 break;
-		        default : voisins.add(mazeList.get(z-1));
-		                  voisins.add(mazeList.get(z+1));
-		                  voisins.add(mazeList.get(z+10));
-		                  voisins.add(mazeList.get(z-10));
+		        default : voisins.add(mazeMatrix[x][y-1]);
+		                  voisins.add(mazeMatrix[x][y+1]);
+		                  voisins.add(mazeMatrix[x-1][y]);
+		                  voisins.add(mazeMatrix[x+1][y]);
 		                  break;
 						}
 			}
-		return (mazeList);
+		return (voisins);
 		}
+	
+
 	
 }
