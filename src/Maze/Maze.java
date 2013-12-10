011d4ab227669d4afda7668fa3be5195ba4d9957
Yogananthan <lefanadeff@hotmail.fr>
2013-12-10 13:13:00 +0100

    Modification du constructeur maze pour prendre en compte largeur/hauteur différentes

--- src/Maze/Maze.java
+++ src/Maze/Maze.java
@@ -13,10 +13,10 @@ import Djikstra.VertexInterface;
 public class Maze implements GraphInterface , MazeViewSource {
 	private MBox[][] mazeMatrix;
 
-	public Maze(int n) {
-		this.mazeMatrix = new MBox[n][n];
-		for (int i = 0; i <= (n - 1); i++) {
-			for (int j = 0; j <= (n - 1); j++) {
+	public Maze(int h,int w) {
+		this.mazeMatrix = new MBox[h][w];
+		for (int i = 0; i <= (h - 1); i++) {
+			for (int j = 0; j <= (w - 1); j++) {
 				mazeMatrix[i][j] = new EBox(i, j);
 			}
 		}
