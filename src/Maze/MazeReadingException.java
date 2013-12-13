eef7a7059fd7c874d60770b34e028329c0366664
Franck Brazier <franck.brazier@telecom-paristech.fr>
2013-12-13 16:51:17 +0100

    Mise en place de MazeReadingException dans le bon package

--- src/Maze/MazeReadingException.java
+++ src/Maze/MazeReadingException.java
@@ -1,5 +1,8 @@
 package Maze;
 
 public class MazeReadingException extends Exception {
+	public MazeReadingException(String fichier,int noLigne, String msgErreur){
+		super(msgErreur+" à la ligne "+noLigne+" dans le fichier "+fichier);
+	}
 
 }
