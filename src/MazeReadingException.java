05378d6cb90393e7eab4beda320ee5e65e6fcb19
Franck Brazier <franck.brazier@telecom-paristech.fr>
2013-12-13 16:46:20 +0100

    Implémentation de la clase MazeReadingException

--- src/MazeReadingException.java
+++ src/MazeReadingException.java
@@ -1,4 +1,7 @@
 
-public class MazeReadingException extends Exception{
+public class MazeReadingException extends Exception {
+	public MazeReadingException(String fichier,int noLigne, String msgErreur){
+		super(msgErreur+" à la ligne "+noLigne+" dans le fichier "+fichier);
+	}
 
 }
